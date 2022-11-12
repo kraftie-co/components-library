e4af55c942d8ef55ef5be5162c018c14cdd5129d
Alex Albu <alexmhh1234@gmail.com>
2022-11-12 19:10:09 +0200

    Add a mockup Header for version 1.01

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Header(props) {
+    return (
+        <div className='header'>
+            <h1 className='logo'>Kraftie</h1>
+            <h2 className='login'>Login</h2>
+        </div>
+    );
+}
